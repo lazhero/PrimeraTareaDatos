eebc05a74cd66f922bc6532b7a98ac86e6523ad4
Andrey <luisandreyzuniga@gmail.com>
2020-02-19 11:52:56 -0600

    Constructores de servidor y comprobaciones iniciales

--- src/Conexiones/Servidor.java
+++ src/Conexiones/Servidor.java
@@ -1,5 +1,40 @@
 package Conexiones;
-
+import java.net.ServerSocket;
+import java.net.InetAddress;
 public class Servidor {
+	private final int port; 
+	private InetAddress address;
+	Servidor(int puerto, int backlog){
+		port=puerto;
+		try {
+			address = InetAddress.getByName("127.0.0.1");
+		}
+		catch (Exception e) {
+			System.out.print("Error");
+			
+		}
+		
+	}
+	Servidor(){
+		address = Servidor.HostExcep("127.0.0.1");
+		port=0;
+		
+		
+	}
+	public static InetAddress HostExcep(String ipString) {
+		boolean flag=true;
+		InetAddress ip=null;
+		while(flag) {
+			try {
+				ip = InetAddress.getByName(ipString);
+				flag=false;
+			}
+			catch(Exception e) {
+				continue;
+			}
+		}
+		return ip;
+	}
+	
 
 }
